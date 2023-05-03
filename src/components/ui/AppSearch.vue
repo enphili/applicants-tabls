0d875bdebb6f064d6a7b59f45cf15ffc0dd5df4d
Konst <blurbinfacebook@yandex.ru>
2023-05-03 21:01:38 +0500

    feat: add search input component

---
+++ src/components/ui/AppSearch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search-wrapper">
+    <img class="search-wrapper__glyphicon glyphicon" src="@/assets/img/search-icon.svg">
+    <label for="search-input">
+      <input id="search-input" type="search" class="search-wrapper__search-input search-input" placeholder="Поиск">
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AppSearch"
+})
+</script>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
     <the-header/>
+
+    <div class="container">
+      <h2 class="main-title">Список заявлений</h2>
+
+      <app-search/>
+
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
+import AppSearch from '@/components/ui/AppSearch.vue'
 
 export default Vue.extend({
   name: 'MainPage',
   components: {
-    TheHeader,
+    TheHeader, AppSearch,
   },
 })
 </script>
