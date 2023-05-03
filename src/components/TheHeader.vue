4d8b54d09b8a5c1a07be381701227e832bd027f1
Konst <blurbinfacebook@yandex.ru>
2023-05-03 20:53:21 +0500

    feat: add header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="header__content">
+        <header class="header__logo-wrapper logo-wrapper">
+          <h1 hidden>SPA приложение для просмотра списка абитуриентов</h1>
+          <div class="logo-wrapper__logo-icon logo-icon">
+            <img src="@/assets/img/logo-icon.svg" alt="logotype">
+          </div>
+          <p class="logo-wrapper__logo-text logo-text">Система таблиц</p>
+        </header>
+        <div class="header__user-wrapper user-wrapper">
+          <div class="user-wrapper__user-text user-text">
+            <p class="user-text__user-name user-name">Светлана</p>
+            <p class="user-text__user-role user-role">Сотрудник</p>
+          </div>
+          <div class="user-wrapper__user-photo user-photo">
+            <img src="@/assets/img/userphoto.png" alt="user photo">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "TheHeader",
+
+})
+</script>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,14 +1,17 @@
 <template>
-
+  <div>
+    <the-header/>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TheHeader from '@/components/TheHeader.vue'
 
 export default Vue.extend({
   name: 'MainPage',
   components: {
-
+    TheHeader,
   },
 })
 </script>
